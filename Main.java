0ed3e9d7da04f6df7a8af37a298489dfaf1c8832
nfedoskin <aka.falcon@bk.ru>
2023-05-05 11:01:23 +0600

    Lessons 4

--- Main.java
+++ Main.java
@@ -1,6 +1,62 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Heelo World");
+        /*
+        int a = 10 * 5;
+        System.out.println(a);
+
+        double b = 20 / 2.5;
+        System.out.println(b);
+
+        int a = 15, b = 9, S, P;
+        S = a * b;
+        P = 2 * (a+b);
+        System.out.println("S = " + S);
+        System.out.println("P = " + P);
+
+        int x = 10;
+        System.out.println("x = " + x);
+        System.out.println("++x = " + ++x);
+        System.out.println("x++ = " + x++);
+        System.out.println("x = " + x);
+
+        int x = 5;
+        x += 5;
+        System.out.println("x = " + x);
+
+        int a;
+        System.out.println("25 = 43 - " + (25 == 43));
+
+        int a = 10, b = 5, c;
+        c = a + b;
+        System.out.println("a + b = " + c);
+
+        c = a - b;
+        System.out.println("a - b = " + c);
+
+        c = a * b;
+        System.out.println("a * b = " + c);
+
+        c = a / b;
+        System.out.println("a / b = " + c);
+
+        c = a % b;
+        System.out.println("a % b = " + c);
+
+        double c,f;
+        f = 79;
+        c = (f - 32) * 5 / 9;
+        System.out.println(c + " °C");
+
+        double a, b;
+        a = (451 + 10) / 3 % ;
+        //Не понятные условия задачи.
+        System.out.println(a);
+        */
+
+        int a = 65, b = 80;
+        boolean c;
+        c = (a < 50) && (a < b);
+        System.out.println("(" + a + " < 50) && (" + a + " < " + b + ") = " + c);
 
     }
 }
