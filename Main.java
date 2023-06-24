a0c8be2bf3ed7d449638c8ff739173a5087727ba
nfedoskin <aka.falcon@bk.ru>
2023-06-24 20:53:42 +0600

    Lessons7

--- Main.java
+++ Main.java
@@ -1,62 +1,96 @@
 public class Main {
-    public static void main(String[] args) {
-        /*
-        int a = 10 * 5;
-        System.out.println(a);
 
-        double b = 20 / 2.5;
-        System.out.println(b);
+    static void minAge (int a, int b, int c) {
+        if (a < b && a < c) {
+            System.out.println("Наименьшее значение: " + a);
+        } else if (b < a && b < c) {
+            System.out.println("Наименьшее значение: " + b);
+        } else {
+            System.out.println("Наименьшее значение: " + c);
+        }
+    }
 
-        int a = 15, b = 9, S, P;
-        S = a * b;
-        P = 2 * (a+b);
-        System.out.println("S = " + S);
-        System.out.println("P = " + P);
+    static void average (int a, int b, int c) {
+        System.out.println("Среднее значение: " + ((a + b + c) / 3));
+    }
 
-        int x = 10;
-        System.out.println("x = " + x);
-        System.out.println("++x = " + ++x);
-        System.out.println("x++ = " + x++);
-        System.out.println("x = " + x);
+    static void vowelNumber (String str1) {
+        int count = 0;
+        String str2 = "AEIOUYaeiouy";
+        for (int i = 0; i < str1.length(); i++) {
+            for (int j = 0; j < str2.length(); j++) {
+                if (str1.charAt(i) == str2.charAt(j)) {
+                    count++;
+                }
+            }
+        }
+        System.out.println("Количество гласных в строке: " + count);
+    }
 
-        int x = 5;
-        x += 5;
-        System.out.println("x = " + x);
+    static void word (String str) {
+        String[] subStr = str.split(" ");
+        System.out.println("Количество слов в строке: " + subStr.length);
+    }
 
-        int a;
-        System.out.println("25 = 43 - " + (25 == 43));
+    static void number (int num) {
+        String str = Integer.toString(num);
+        int a = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if ( str.charAt(i) == '2')
+                a++;
+        }
+        System.out.println("Количество цифр 2 в числе: " + a);
+    }
 
-        int a = 10, b = 5, c;
-        c = a + b;
-        System.out.println("a + b = " + c);
+    static void vowels (String str1) {
+        boolean a = true;
+        String str2 = "AEIOUYaeiouy";
+        for (int i = 0; i < str1.length(); i++) {
+            if (a == true) {
+                for (int j = 0; j < str2.length(); j++) {
+                    if (str1.charAt(i) != str2.charAt(j))
+                        a = false;
+                    else {
+                        a = true;
+                        break;
+                    }
+                }
+            }
+            else break;
+        }
+        System.out.println("Ожидаемый результат: " + a);
+    }
 
-        c = a - b;
-        System.out.println("a - b = " + c);
+    static void area (int n, int len) {
+        double s = 0;
+        s = (n / 4 * Math.pow(len, 2) * (1.0 / Math.tan(Math.PI / n)));
+        System.out.println("Площадь пятиугольника равна " + s);
+    }
 
-        c = a * b;
-        System.out.println("a * b = " + c);
+    static void sumOfNumber (String str) {
+        int sum = 0;
+        for (int i = 0; i < str.length(); i++) {
+            sum += Character.digit(str.charAt(i), 10);
+        }
+        System.out.println("Сумма = " + sum);
+    }
 
-        c = a / b;
-        System.out.println("a / b = " + c);
+    public static void main (String [] args) {
 
-        c = a % b;
-        System.out.println("a % b = " + c);
+        minAge(25, 37, 29);
 
-        double c,f;
-        f = 79;
-        c = (f - 32) * 5 / 9;
-        System.out.println(c + " °C");
+        average(25, 45,65);
 
-        double a, b;
-        a = (451 + 10) / 3 % ;
-        //Не понятные условия задачи.
-        System.out.println(a);
-        */
+        vowelNumber("DarTech123");
 
-        int a = 65, b = 80;
-        boolean c;
-        c = (a < 50) && (a < b);
-        System.out.println("(" + a + " < 50) && (" + a + " < " + b + ") = " + c);
+        word("Java is good to learn Object Oriented programming.");
 
+        number(1254212);
+
+        vowels("AIEEE");
+
+        area(5, 6);
+
+        sumOfNumber("252");
     }
 }
